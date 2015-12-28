e6572701a3ce80b9c4ea7c0f5a3387817a30515a
Wei lun Lai <oliverlai55@gmail.com>
2015-12-28 00:56:31 -0500

    making js file cleaner

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,38 @@
 $(document).ready(function() { 
-    /*============================================
-    Page Preloader
-    ==============================================*/
-    
+
+// Page Preloader
     $(window).load(function(){
         $('#page-loader').fadeOut(500);
     }); 
-        /*============================================
-    Navigation Functions
-    ==============================================*/
+
+// ======== Navigation Functions ============
+if ($(window).scrollTop()===0){
+    $('#main-nav').removeClass('scrolled');
+}
+else{
+    $('#main-nav').addClass('scrolled');    
+}
+
+$(window).scroll(function(){
     if ($(window).scrollTop()===0){
         $('#main-nav').removeClass('scrolled');
     }
     else{
         $('#main-nav').addClass('scrolled');    
     }
+});
 
-    $(window).scroll(function(){
-        if ($(window).scrollTop()===0){
-            $('#main-nav').removeClass('scrolled');
-        }
-        else{
-            $('#main-nav').addClass('scrolled');    
-        }
-    });
 
-    /*============================================
-    ScrollTo Links
-    ==============================================*/
-    $('a.scrollto').click(function(e){
-        $('html,body').scrollTo(this.hash, this.hash, {gap:{y:-80}});
-        e.preventDefault();
+// ======== ScrollTo Links ===========
+ 
+$('a.scrollto').click(function(e){
+    $('html,body').scrollTo(this.hash, this.hash, {gap:{y:-80}});
+    e.preventDefault();
 
-        if ($('.navbar-collapse').hasClass('in')){
-            $('.navbar-collapse').removeClass('in').addClass('collapse');
-                }
-    });
+    if ($('.navbar-collapse').hasClass('in')){
+        $('.navbar-collapse').removeClass('in').addClass('collapse');
+    }
+});
 
 
     /*============================================
