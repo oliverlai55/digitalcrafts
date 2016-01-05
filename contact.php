6791450708d864ca2b6c7b2313d7d03e12278b9a
Wei lun Lai <oliverlai55@gmail.com>
2016-01-04 23:36:26 -0500

    editing more description of portfolio and minor PHP changes

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php
 $emailTo = 'oliverlai55@gmail.com'; 
-$siteTitle = 'Oliver Lai"s Page';
+$siteTitle = "Oliver Lai's Page";
 
 error_reporting(E_ALL ^ E_NOTICE); // hide all basic notices from PHP
 
